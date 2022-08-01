e409c435774aed49a8d599085910ffaba74afc8c
TangiroKamado <strikerslite12@gmail.com>
2022-08-01 22:14:42 +0600

    site completed

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".burger__menu");
+
+    burgermenu.addEventListener("click", () => {
+        burger,classList.toggle("active")
+    })
